151a5c8aea7013a540b4bf466933583f97a730e6
vic8tor <timongrinni@gmail.com>
2022-02-19 12:45:52 +0300

    Initial Commit

---
+++ SliderRGB/SliderRGBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderRGBApp.swift
+//  SliderRGB
+//
+//  Created by Victor on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderRGBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
